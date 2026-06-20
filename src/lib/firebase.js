2072d88131abc519878bbb2e32ac73bbce082bf5
ypqun <ypun0929@gmail.com>
2026-06-21 02:00:07 +0900

    fix base path2

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDp43P5AGP6bXNXAcy_f7M_m6he-4U-2Dk",
   authDomain: "studyring-398fc.firebaseapp.com",
@@ -16,9 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "394337507689",
   appId: "1:394337507689:web:ecdc2bd58b238f0b91c5f8"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(firebaseConfig)
 
